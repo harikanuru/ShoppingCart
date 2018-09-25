54608f1403c333858a93803603607985749aed9e
Vasanth Varma <vasanth.varma@macys.com>
2018-09-25 16:56:29 +0530

    Removed Display Item Image in Checkout

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -5,7 +5,7 @@ const CartItem = ({ name, price,image, currency, onClick }) => {
     return (
         <div className="cart-item">
             <div>
-                <button className="btn btn-danger btn-xs" onClick={onClick}><img src={image}   width="50" height="50"/></button>
+                <button className="btn btn-danger btn-xs" onClick={onClick}>X</button>
                 <span className="cart-item__name">{name}</span>
             </div>
             <div className="cart-item__price"><img src='../images/rupees.jpg'  width="20" height="20"/>{price}</div>
